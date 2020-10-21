62eafd20dfee4f440b9ae3f9a8ab53bdb39db6ec
Robert Tomczuk <robert.tomczuk94@gmail.com>
2020-10-21 19:32:02 +0200

    Updates

---
+++ precache-manifest.93dd7d02e5ee5acca9b655af1df5b649.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a89e66b6368e45febb2e57fdfb04888",
+    "url": "/dnd-app/index.html"
+  },
+  {
+    "revision": "51a3de642d413563738c",
+    "url": "/dnd-app/static/css/main.06244749.chunk.css"
+  },
+  {
+    "revision": "3676117930ead0d1bb51",
+    "url": "/dnd-app/static/js/2.c77f4784.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/dnd-app/static/js/2.c77f4784.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51a3de642d413563738c",
+    "url": "/dnd-app/static/js/main.e47d7d04.chunk.js"
+  },
+  {
+    "revision": "0f19d008c062e67a1f15",
+    "url": "/dnd-app/static/js/runtime-main.07a1bfbc.js"
+  },
+  {
+    "revision": "5878981c2f6bd0cf9523027e2d33ff69",
+    "url": "/dnd-app/static/media/bg1.1.5878981c.jpg"
+  },
+  {
+    "revision": "974453d20c0e0f7a3841fe8a6964e4cf",
+    "url": "/dnd-app/static/media/scroll1.1.974453d2.png"
+  },
+  {
+    "revision": "7d966046ccb3d51b8180cef631f3ba93",
+    "url": "/dnd-app/static/media/scroll1.7d966046.png"
+  }
+]);
